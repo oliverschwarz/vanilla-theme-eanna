88ba0f2c02688b358eef957b47fb37d06d751e86
Oliver Schwarz <Oliver Schwarz@192.168.2.101>
2014-09-12 14:50:30 +0200

    Adding about.php and CSS file

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php if (!defined('APPLICATION')) exit();
+
+/**
+ * Eanna Theme Info
+ * 
+ * @package VanillaTheme
+ * @author  Oliver Schwarz <oliver@eanna.de>
+ */
+
+/**
+ * Theme info
+ */
+$ThemeInfo['Eanna'] = array(
+    'Name' => 'Eanna',
+    'Description' => 'An ArcheAge artwork inspired theme to be used on the german ArcheAge community forums.',
+    'Version' => '0.1.1',
+    'Author' => 'Oliver Schwarz',
+    'AuthorEmail' => 'oliver@eanna.de',
+    'AuthorUrl' => 'http://eanna.de'
+);
+
+
